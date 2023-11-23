d0f75e0d5681e42cec44e80f22ff12791910828e
tuanntrieu <trieudangtuan18032003@Gmail.com>
2023-11-23 18:49:53 +0700

    BTVN Buoi5

---
+++ src/Bai2/NguoiMua.java
@@ -0,0 +1,11 @@
+package Bai2;
+
+public class NguoiMua extends Nguoi{
+
+    public NguoiMua() {
+    }
+
+    public NguoiMua(String hoTen, String maSoThue, String diaChi, String soTK) {
+        super(hoTen, maSoThue, diaChi, soTK);
+    }
+}
